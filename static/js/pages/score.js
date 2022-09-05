7f1a2d235300934b76cb4255107ec432dca69b73
luis10barbo <luis10barbo2020@gmail.com>
2022-09-05 15:39:00 -0300

    added score.js script

---
+++ static/js/pages/score.js
@@ -0,0 +1,28 @@
+new Vue({
+    el:"#app",
+    delimiters: ["<%", "%>"],
+    data() {
+        return {
+            scoreId : scoreId,
+            scoreInfo : {}
+        }
+    },
+    created() {
+        this.LoadScore(scoreId)
+    },
+    methods: {
+        LoadScore(scoreId) {
+            this.$set(this, 'scoreId', scoreId)
+
+            const params = {
+                id : this.scoreId
+            }
+
+            this.$axios.get(`${window.location.protocol}//api.${domain}/get_score_info`, { params }).then(res => {
+                this.scoreInfo = res.data.leaderboard;
+                // this.$set(this, 'load', false);
+            });
+            window.history.replaceState('', document.title, `/score/${this.scoreId}`);
+        }
+    }
+})
